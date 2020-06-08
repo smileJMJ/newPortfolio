c749b7f15e70061c63c88e4d53a4f2b200967000
smileJMJ <mjj0324@gmail.com>
2020-06-09 00:46:47 +0900

    project view 작업중

--- src/project/view/index.tsx
+++ src/project/view/index.tsx
@@ -1,24 +1,26 @@
 import React, {useEffect, useState} from 'react';
+import { observer } from 'mobx-react';
 import ProjectViewVM from './viewModel/projectViewVM';
 
+
 const projectViewVM = new ProjectViewVM();
-const View = (props: any) => {
+const View = observer((props: any) => {
     const id = props.match.params.id;
     const {projectContent} = projectViewVM;
 
     useEffect(() => {
         projectViewVM.load(id);
     }, []);
-console.log(projectContent);
+
     return(
         <>
             {   projectContent &&
                 <div>
-                    <h1>{projectContent}</h1>
+                    <h1>{projectContent.title}</h1>
                 </div>
             }
         </>
     );
-};
+});
 
 export default View;
