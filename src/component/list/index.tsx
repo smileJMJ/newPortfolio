f57fb5e57525a5ef4ca300ff3a6657888ab9af27
smileJMJ <mjj0324@gmail.com>
2020-06-21 23:21:11 +0900

    storybook 셋팅 및 component 설계

---
+++ src/component/list/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from './List.scss';
+
+const List = () => {
+    return(
+        <div className={css.listWrap}>
+            <ul>
+
+            </ul>
+        </div>
+    )
+};
+
+export default List;
