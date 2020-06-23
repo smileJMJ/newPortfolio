a075564c149078496cc484e6ef65c711c9838337
smileJMJ <mjj0324@gmail.com>
2020-06-24 08:05:54 +0900

    projectList 스타일 작업

--- src/component/projectList/view/ListItem.tsx
+++ src/component/projectList/view/ListItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import {IProjectListItem} from "../types";
+import css from './ListItem.scss';
 
 interface IProps {
     data: IProjectListItem;
@@ -9,13 +10,18 @@ interface IProps {
 const ListItem = (props: IProps) => {
     const {data} = props;
     return(
-        <Link to={data['url']}>
-            <dl>
-                <dt>{data['title']}</dt>
-                <dd>{data['term']}</dd>
-                <dd>{data['category']}</dd>
-                <dd><img src={data['thumbnail']} alt=""/></dd>
+        <Link to={data['url']} className={css.listItem}>
+            <dl className={css.title}>
+                <dt>
+                    <strong>{data['title']}</strong>
+                    <span>{data['category']}</span>
+                </dt>
+                <dd className={css.term}>{data['term']}</dd>
+                <dd className={css.link}>View case <i></i></dd>
             </dl>
+            <figure className={css.thumb}>
+                <img src={data['thumbnail']} alt=""/>
+            </figure>
         </Link>
     );
 };
